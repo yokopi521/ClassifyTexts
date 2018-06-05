3654082e87726af09643efdea3ed02b80ec46258
yokopi521 <yokopi521@gmail.com>
2018-06-06 00:42:05 +0900

    succeeded to classift text

--- DIctionary/ViewController.swift
+++ DIctionary/ViewController.swift
@@ -14,41 +14,55 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let text = "エンゼルスの大谷翔平投手（２３）は２６日（日本時間２７日）、敵地ニューヨークでのヤンキース戦に「５番・ＤＨ」でスタメン出場。２三振するなど４打数無安打１四球で、メジャーで初めて２試合連続無安打に終わり、打率は・２９７となり３割を切った。チームは１１―４で快勝した。２７日（同２８日）は打者としてヤ軍の田中将大投手（２９）と初対決する予定。試合後、大谷は「先制点は取られましたが、なんとか逆転して後半もいい流れで追加点を取りながら勝てたので。すごいいいゲームだったと思います」とチームの勝利に納得した表情。自身は前日２５日（同２６日）同様に敵地ファンに大ブーイングで迎えられ、初回２死三塁からの第１打席は空振り三振。３回１死満塁からの第２打席は、３ボール１ストライクから押し出し四球を選び、３試合ぶりの打点を挙げたが、５回無死からの第３打席は、２ストライクから高めの直球に空振り三振。６回無死一、三塁からの第４打席は遊ゴロ併殺打、８回１死からの第５打席も遊ゴロに倒れた。いずれも外角中心の配球で、無安打に抑えられ「昨日から正面の当たりが多いですが、そんなに悪くはないかなとは思っています。ゲッツーに関しては外野フライの方がいい場面なので。それの方が良かったけど、まあ１点入ったという結果に関しては良かったかなと思っています」と振り返った。また、相手の守備シフトで打ち取られた打球については「抜けることもありますし。逆にいい打球が正面にいっているということは、自分の打撃をしっかり、打席の中でいいスイングができているのかなと思っている。目先の１安打も大事だと思うんですけど、継続して取り組むところもすごい大事じゃないかなと思っています」と強調。次戦に向けて「自分が思っている以上に、（相手バッテリーが）慎重に慎重にという配球になっているのかなとは思うので。そこも踏まえながら、明日もう１回組み立てていきたいなと思います」と話した。"
-        print(tfidf(text: text).shape)
+        let text = "プロ５年目、１５試合目の登板で初勝利を狙った楽天・古川侑利投手（２２）が、勝ち投手の権利を持って降板した。５回９８球を投げて８三振を奪い、６安打１失点。１点リードの６回には２番手・宋家豪にマウンドを譲った。５月２９日のＤｅＮＡ戦（横浜）以来中６日で、今季２試合目の先発となった古川。初回に先頭の陽岱鋼にいきなり二塁打を浴びて、続く田中俊に四球を与え無死一、二塁のピンチを迎えたが、後続を打ち取って波に乗った。２回以外は毎回走者を背負ったが、失った点は５回に浴びた岡本の適時打による１点のみ。１点差に迫られた５回２死一、三塁ではマギーをこの日最速１４９キロの直球で空振り三振に切って、大きなガッツポーズを見せた。「交流戦、巨人１－４楽天」（５日、東京ドーム）楽天が先発の古川侑利投手（２２）が５回１失点と好投し、プロ初勝利を挙げた。チームは今季の交流戦初白星となった。古川は初回に陽の二塁打と田中俊への四球でピンチを招いたが、岡本、亀井の４、５番から連続で三振を奪ってリズムに乗った。五回には２点リードの五回には、１点を失いなおも２死満塁のピンチで、迎えたマギーを空振り三振にきってとり、窮地を脱した。５回を投げ６安打８奪三振と巨人打線を相手に踏ん張り、救援陣に後を託した。打線は三回に茂木の２ラン、八回の田中の適時打などで４点を挙げた。ウイニングボールを手にカメラマンのフラッシュを浴びた古川。ヒーローインタビューでは「本当に、心の底かうれしかったです。いやあ…本当、長かったですね。やっと勝てて、プロ野球選手になったなという気持ちです」と胸の内を語った。ウイニングボールは、「まずは奧さんに。その後は実家に送りたいと思います」と感謝の思いを込めていた。「交流戦、巨人１－４楽天」（５日、東京ドーム）巨人が今季最多タイ１２残塁の拙攻で、今季３度目の３連敗を喫した。４試合連続の２桁残塁となった。ローテーションの柱となることを期待されている山口俊が先発した。三回に茂木に浴びた２ラン以外は安定した投球を見せていたが、八回に２つの四球を許し、１死一、二塁から１番・田中に一、二塁間を破られる適時打を浴びた。７回１／３を３安打ながら４失点で、降板した。山口俊以上に打線が楽天の投手陣を助けてしまった。初勝利を狙う古川から６本のヒットを打ちながらも得点につながったのは五回の岡本の適時打だけ。合計１２残塁とあと１本が出ないまま、試合が終わってしまった。４番に座って３試合目の岡本は、適時打を含む２安打と３試合連続となるマルチ安打を記録したが、打線としてはつながりを欠いた。ヤクルトが3年ぶりの6連勝をマークし、ソフトバンクと並び交流戦首位に浮上。山田哲、バレンティン、青木に豪快弾が飛び出すなど、15安打12得点と打線が爆発した。ヤクルトは1点を追う2回、1番山田哲が14号3ランを放ち逆転。3回には4番バレンティンが14号ソロを放ち5-2とリードを広げた。4回にも、2番青木に3号3ランが飛び出し8-3。その後も5番雄平、6番西浦がそれぞれ適時二塁打を放ち、4回は打者10人を送り込む猛攻で一気に7点を加えた。先発の石川は、序盤に2本塁打を浴びるも6回を投げ6安打3失点。打線の大量援護にも恵まれ、約2カ月ぶりの白星となる、今季3勝目（2敗）を手にした。ソフトバンクは、投手陣が序盤から崩れ連勝は6でストップ。先発の摂津が3回5失点（自責4）で今季初黒星（1勝）。2番手・岡本も1イニング7失点と崩れ、4回までに12失点を喫した。"
+        getCategory(content: text)
+    }
+    
+    func getCategory(content: String) {
+        if #available(iOS 11.0, *) {
+            let vec = self.tfidf(text: content)
+            print(vec)
+            do {
+                let prediction = try CategoryClassifier2().prediction(message: vec).what_the_category_is
+                print(prediction)
+            } catch {
+                print("error")
+            }
+        } else {
+            print("error")
+        }
     }
     
     func tfidf(text: String) -> MLMultiArray {
-        let tagger = NSLinguisticTagger(tagSchemes: NSLinguisticTagger.availableTagSchemes(forLanguage: "en"), options: 0)
+        let tagger = NSLinguisticTagger(tagSchemes: NSLinguisticTagger.availableTagSchemes(forLanguage: "ja"), options: 0)
         var wordList: [String] = []
         var idfDic = [String : Float]()
         var tfDic = [String : Float]()
         var tfidfDic = [String : Float]()
-        
+
         //--------------------tf-idfを求めてる-----------------
         tagger.string = text
         tagger.enumerateTags(in: NSRange(location: 0, length: text.characters.count), scheme: .tokenType, options: [.omitPunctuation, .omitWhitespace, .omitOther]) { tag, tokenRange, sentenceRange, _ in
-            
             let subString = (text as NSString).substring(with: tokenRange)
             wordList.append(subString as String)
         }
-        
-        //        print(wordList)
+
+        print(wordList)
         let path = Bundle.main.path(forResource: "dictionary", ofType: "json")
         do {
             let jsonStr = try String(contentsOfFile: path!)
             let json = JSON.parse(jsonStr)
-            
+
             for word in wordList {
-                if json[word] == nil {
-                    idfDic[word] = 0
+                if json[word] == JSON.null {
+                    idfDic[word] = 1
                 } else {
-                    idfDic[word] = json[word].float
+                    idfDic[word] = json[word].float! + 1.0
                 }
-                tfDic[word] = Float(wordList.filter({$0 == word}).count) / Float(wordList.count)
+                tfDic[word] = Float(wordList.filter({$0 == word}).count)
             }
-            print(idfDic.count)
-            print(tfDic.count)
+            print(idfDic)
+            print(tfDic)
             for word in wordList {
                 tfidfDic[word] = tfDic[word]! * idfDic[word]!
             }
@@ -66,25 +80,18 @@ class ViewController: UIViewController {
             for i in 0..<wordsData.count {
                 let word = wordsData[i]
                 if tfidfDic[word] != nil {
-                    vectorized[i] = tfidfDic[word]! as NSNumber
+                    vectorized[i] = NSNumber(value: tfidfDic[word]!)
                 } else {
                     vectorized[i] = 0
                 }
             }
-    
+
             return vectorized
-            
+
         } catch {
             print("error")
             return MLMultiArray()
         }
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
